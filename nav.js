be8e6ce1ecde9701d183ac947e84d4e334495f1a
Immanuel <immanuel.windyanto@gmail.com>
2026-07-30 12:15:51 +0700

    Add files via upload

---
+++ nav.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+  var toggle = document.getElementById('navToggle');
+  var links = document.getElementById('navLinks');
+  if (toggle && links) {
+    toggle.addEventListener('click', function () {
+      links.classList.toggle('open');
+    });
+  }
+});
